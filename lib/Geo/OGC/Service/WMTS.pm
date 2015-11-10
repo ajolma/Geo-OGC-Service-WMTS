67c8234b8cfadb19e7f15c469c87b137329948dc
Ari Jolma <ari.jolma@gmail.com>
2015-11-10 13:26:09 +0200

    Some documentation. %projections hash. Proper WMTS capabilities document. NB: Has a lot in common with WFS capabilities, will move something into Geo::OGC::Service. Clip a tile from a file. This needs GDAL 2.1.

--- lib/Geo/OGC/Service/WMTS.pm
+++ lib/Geo/OGC/Service/WMTS.pm
@@ -7,7 +7,27 @@ Geo::OGC::Service::WMTS - Perl extension to create geospatial web map tile servi
 =head1 SYNOPSIS
 
 The process_request method of this module is called by the
-Geo::OGC::Service framework.
+Geo::OGC::Service framework. 
+
+In a psgi script write something like
+
+ use Geo::OGC::Service::WMTS;
+
+ my $ogc = Geo::OGC::Service->new({
+ config => '/var/www/etc/OGC-services.conf',
+ services => {
+        'WFS' => 'Geo::OGC::Service::WFS',
+        'WMTS' => 'Geo::OGC::Service::WMTS',
+        'WMS' => 'Geo::OGC::Service::WMTS',
+        'TMS' => 'Geo::OGC::Service::WMTS',
+ }});
+
+ builder {
+    mount "/WFS" => $ogc->to_app;
+    mount "/WMTS" => $ogc->to_app;
+    mount "/TMS" => $ogc->to_app;
+    mount "/" => $default;
+ };
 
 =head1 DESCRIPTION
 
@@ -15,6 +35,22 @@ This module aims to provide the operations defined by the Open
 Geospatial Consortium's Web Map Tile Service standard.
 
 This module is a plugin for the Geo::OGC::Service framework.
+
+Additionally, this module aims to support WMS used as WMTS and TMS.
+
+=head1 CONFIGURATION
+
+The configuration is defined similarly as to other services under
+Geo::OGC::Service, either as a file or as a variable in the call to
+Geo::OGC::Service->new.
+
+The file must be JSON and either have top level key WMTS, WMS, or TMS
+if more than one service is defined. The value of the key must be a
+hash, or the name of a key, which has a hash value.
+
+Known top level keys in the hash are 'resource', 'blank', 'debug', and
+'TileSets'. TileSets is an array of hashes. The keys of a TileSet hash
+are Layers, Format, Resolutions, SRS, BoundingBox, path, and ext.
 
 =head2 EXPORT
 
@@ -44,40 +80,42 @@ use XML::LibXML::PrettyPrint;
 
 our $VERSION = '0.01';
 
-# http://www.microimages.com/documentation/TechGuides/78googleMapsStruc.pdf
-# pixel size at equator in meters
-our @resolutions_3857 = (156543.03390000000945292413,
-                        78271.51695000000472646207,
-                        39135.75847500000236323103,
-                        19567.87923750000118161552,
-                        9783.93961875000059080776,
-                        4891.96980937500029540388,
-                        2445.98490468750014770194,
-                        1222.99245234375007385097,
-                        611.49622617187503692548,
-                        305.74811308593751846274,
-                        152.87405654296875923137,
-                        76.43702827148437961569,
-                        38.21851413574218980784,
-                        19.10925706787109490392,
-                        9.55462853393554745196,
-                        4.77731426696777372598,
-                        2.38865713348388686299,
-                        1.19432856674194343150,
-                        0.59716428337097171575,
-                        0.29858214168548585787);
+our $radius_of_earth_at_equator = 6378137;
+our $standard_pixel_size = 0.28 / 1000;
+our $tile_width = 256;
+our $tile_height = 256;
+our $originShift3857 = pi * $radius_of_earth_at_equator;
 
-our $bounding_box_3857 = {SRS => 'EPSG:3857', 
-                         minx => -20037508.34,
-                         miny => -20037508.34,
-                         maxx => 20037508.34,
-                         maxy => 20037508.34};
+our %projections = (
+    'EPSG:3857' => {
+        identifier => 'EPSG:3857',
+        crs => 'urn:ogc:def:crs:EPSG:6.3:3857',
+        extent => {
+            SRS => 'EPSG:3857', 
+            minx => -1 * $originShift3857,
+            miny => -1 * $originShift3857,
+            maxx => $originShift3857,
+            maxy => $originShift3857  },
+    },
+    'EPSG:3067' => {
+        identifier => 'ETRS-TM35FIN',
+        crs => 'urn:ogc:def:crs:EPSG:6.3:3067',
+        extent => { 
+            SRS => 'EPSG:3067', 
+            # JHS180 liite 1:
+            minx => -548576,
+            miny => 6291456,
+            maxx => 1548576,
+            maxy => 8388608 }
+    }
+);
 
 =pod
 
 =head3 process_request
 
-The entry method into this service. Fails unless the request is well known.
+The entry method into this service. Falls back to TMS is service is
+TMS.
 
 =cut
 
@@ -96,12 +134,17 @@ sub process_request {
             } });
         return;
     }
-    if ($self->{debug} > 2) {
-        $self->log($self);
+    if ($self->{debug}) {
+        if ($self->{debug} > 2) {
+            $self->log($self);
+        } else {
+            $self->log($self->{parameters});
+        }
     }
     my $response;
     for ($self->{request} // '') {
-        if (/^GetCapabilities/ or /^capabilities/) { $self->GetCapabilities() }
+        if ($self->{service} eq 'TMS' and (/^GetCapabilities/ or /^capabilities/)) { $self->WMSGetCapabilities() }
+        elsif (/^GetCapabilities/ or /^capabilities/) { $self->GetCapabilities() }
         elsif (/^GetTile/)                         { $response = $self->GetTile() }
         elsif (/^GetMap/)                          { $response = $self->GetMap() }
         elsif ($self->{service} eq 'TMS')          { $response = $self->TMS() }
@@ -117,7 +160,106 @@ sub process_request {
     $self->{responder}->($response) if $response;
 }
 
+=pod
+
+=head3 GetCapabilities
+
+Sends the capabilities document according to WMTS standard if the
+requested service is WMTS and according to WMS if the requested
+service is WMS.
+
+=cut
+
 sub GetCapabilities {
+    my ($self) = @_;
+    my $writer = Geo::OGC::Service::XMLWriter::Caching->new();
+    $writer->open_element(Capabilities => { 
+        version => '1.0.0',
+        xmlns => "http://www.opengis.net/wmts/1.0",
+        'xmlns:ows' => "http://www.opengis.net/ows/1.1",
+        'xmlns:xlink' => "http://www.w3.org/1999/xlink",
+        'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
+        'xmlns:gml' => "http://www.opengis.net/gml",
+        'xsi:schemaLocation' => "http://www.opengis.net/wmts/1.0 ".
+            "http://schemas.opengis.net/wmts/1.0/wmtsGetCapabilities_response.xsd",
+    });
+    $writer->element('ows:ServiceIdentification', 
+                     [['ows:Title' => $self->{config}{Title} // 'Yet another WMTS server'],
+                      ['ows:Abstract'],
+                      ['ows:ServiceType', {codeSpace=>"OGC"}, 'OGC WMTS'],
+                      ['ows:ServiceTypeVersion', $self->{config}{ServiceTypeVersion} // '1.0.0'],
+                      ['ows:Fees' => $self->{config}{Fees} // 'NONE'],
+                      ['ows:AccessConstraints' => $self->{config}{AccessConstraints} // 'NONE']]);
+    $writer->element('ows:ServiceProvider',
+                     [['ows:ProviderName' => $self->{config}{ProviderName} // 'Nobody in particular'],
+                      ['ows:ProviderSite', { 'xlink:type'=>"simple", 
+                                             'xlink:href' => $self->{config}{ProviderSite} // '' }],
+                      ['ows:ServiceContact' => $self->{config}{ServiceContact}]]);
+    $self->OperationsMetadata($writer, [qw/GetCapabilities GetTile GetFeatureInfo/]);
+    $writer->open_element(Contents => {});
+    for my $set (@{$self->{config}{TileSets}}) {
+        my $projection = $projections{$set->{SRS}};
+        $writer->element('Layer' => [
+                             [ 'ows:Title' => $set->{Title} // $set->{Layers} ],
+                             [ 'ows:Identifier' => $set->{Layers} ],
+                             [ 'Style' => { isDefault => 'true' }, [ 'ows:Identifier' => 'default' ] ],
+                             [ Format => $set->{Format} ],
+                             [ TileMatrixSetLink => [ TileMatrixSet => $projection->{identifier} ] ],
+                             [ ResourceURL => { 
+                                 resourceType => 'tile',
+                                 'format' => $set->{Format},
+                                 template => $self->{config}{resource} }]
+                         ]
+            )
+    };
+    for my $projection (keys %projections) {
+        tile_matrix_set($writer, $projections{$projection}, [0..15]);
+    }
+    $writer->close_element;
+    $writer->close_element;
+    $writer->stream($self->{responder});
+}
+
+sub OperationsMetadata  {
+    my ($self, $writer, $operations) = @_;
+    $writer->open_element('ows:OperationsMetadata');
+    my @versions = split /,/, $self->{config}{AcceptVersions} // '2.0.0,1.1.0,1.0.0';
+    for my $operation (@$operations) {
+        $self->Operation( $writer, $operation,
+                          { GET => 1 },
+                          { GET => [ 'ows:AllowedValues' => ['ows:Value' => 'KVP' ] ] },
+                          [ { service => ['WFS'] }, 
+                            { AcceptVersions => \@versions }, 
+                            { AcceptFormats => ['text/xml'] }
+                          ]
+            );
+    }
+    $writer->close_element;
+}
+
+sub Operation {
+    my($self, $writer, $operation, $protocols, $constraints, $parameters) = @_;
+    my @parameters;
+    for my $p (@$parameters) {
+        for my $n (keys %$p) {
+            my @values;
+            for my $v (@{$p->{$n}}) {
+                push @values, ['ows:Value', $v];
+            }
+            push @parameters, ['ows:Parameter', {name=>$n}, \@values];
+        }
+    }
+    my $constraint;
+    $constraint = [ 'ows:Constraint' => {name => 'GetEncoding'}, $constraints->{GET} ] if $constraints->{GET};
+    my @http;
+    push @http, [ 'ows:Get' => { 'xlink:type'=>'simple', 'xlink:href'=>$self->{config}{resource} }, $constraint ]
+        if $protocols->{GET};
+    push @http, [ 'ows:Post' => { 'xlink:type'=>'simple', 'xlink:href'=>$self->{config}{resource} } ]
+        if $protocols->{POST};
+    $writer->element('ows:Operation' => { name => $operation }, [['ows:DCP' =>['ows:HTTP' => \@http ]], @parameters]);
+}
+
+sub WMSGetCapabilities {
     my ($self) = @_;
 
     my $writer = Geo::OGC::Service::XMLWriter::Caching->new();
@@ -152,11 +294,17 @@ sub GetCapabilities {
     for my $set (@{$self->{config}{TileSets}}) {
         my($i0,$i1) = split /\.\./, $set->{Resolutions};
         #my @resolutions = @resolutions_3857[$i0..$i1]; # with this QGIS starts to ask higher resolution tiles
-        my @resolutions = @resolutions_3857;
+        #my @resolutions = @resolutions_3857;
+        my @resolutions;
+        my $projection = $projections{$set->{SRS}};
+        my $extent_width = $projection->{extent}{maxx} - $projection->{extent}{minx};
+        for my $i (0..19) {
+            $resolutions[$i] = $extent_width/(2**$i * $tile_width);
+        }
         $writer->element(VendorSpecificCapabilities => 
                          [TileSet => [[SRS => $set->{SRS}],
                                       #[BoundingBox => $set->{BoundingBox}], # with this QGIS does not show tiles at correct locations
-                                      [BoundingBox => $bounding_box_3857],
+                                      [BoundingBox => $projection->{extent}],
                                       [Resolutions => "@resolutions"],
                                       [Width => $set->{Width} || 256],
                                       [Height => $set->{Height} || 256],
@@ -169,13 +317,14 @@ sub GetCapabilities {
                      {SupportSLD => 0, UserLayer => 0, UserStyle => 0, RemoteWFS => 0});
 
     for my $set (@{$self->{config}{TileSets}}) {
+        my $projection = $projections{$set->{SRS}};
         $writer->element(Layer => [[Title => 'TileCache Layers'],
                                    [Layer => {queryable => 0, opaque => 0, cascaded => 1}, 
                                     [[Name => $set->{Layers}],
                                      [Title => $set->{Layers}],
                                      [SRS => $set->{SRS}],
                                      [Format => $set->{Format}],
-                                     [BoundingBox => $bounding_box_3857],
+                                     [BoundingBox => $projection->{extent}],
                                      #[BoundingBox => $set->{BoundingBox}] # with this QGIS does not show tiles at correct locations
                                     ]]
                          ]);
@@ -185,6 +334,14 @@ sub GetCapabilities {
     $writer->close_element;
     $writer->stream($self->{responder});
 }
+
+=pod
+
+=head3 GetMap
+
+Sends the requested tile based on parameters BBOX and LAYERS.
+
+=cut
 
 sub GetMap {
     my ($self) = @_;
@@ -207,13 +364,20 @@ sub GetMap {
                        locator => 'LAYERS' });
         return;
     }
+
+    my $projection = $projections{$set->{SRS}};
+    my @resolutions;
+    my $extent_width = $projection->{extent}{maxx} - $projection->{extent}{minx};
+    for my $i (0..19) {
+        $resolutions[$i] = $extent_width/(2**$i * $tile_width);
+    }
     
     my @bbox = split /,/, $self->{parameters}{bbox}; # minx, miny, maxx, maxy
     my $units_per_pixel = ($bbox[2]-$bbox[0])/256;
     my $z;
     my $res;
     my $i = 0;
-    for my $r (@resolutions_3857) {
+    for my $r (@resolutions) {
         if (abs($r - $units_per_pixel) < 0.1) {
             $res = $r;
             $z = $i;
@@ -253,6 +417,15 @@ sub GetMap {
         ];
 }
 
+=pod
+
+=head3 GetMap
+
+Sends the requested tile based on parameters Layer, Tilerow, Tilecol,
+Tilematrix, Tilematrixset, and Format.
+
+=cut
+
 sub GetTile {
     my ($self) = @_;
     for my $param (qw/layer tilerow tilecol tilematrix tilematrixset format/) {
@@ -275,23 +448,51 @@ sub GetTile {
         return;
     }
 
+    my $file;
 
-    my $layer = $self->{parameters}{layers};
+    if ($set->{file}) {
 
-    # get x, y, z from tilerow tilecol tilematrix tilematrixset
-    my $x;
-    my $y;
-    my $z;
+        # clip from file, Translate probably needs 2.1
+        my $ds = Geo::GDAL::Open($set->{file});
+        my $projection = $projections{$set->{SRS}};
+        my $extent_width = $projection->{extent}{maxx} - $projection->{extent}{minx};
+        my $extent_height = $projection->{extent}{maxy} - $projection->{extent}{miny};
+        my $matrix_width = 2**$self->{parameters}{tilematrix};
+        my $width = $extent_width/$matrix_width;
+        my $height = $extent_height/$matrix_width;
+        my $minx = $projection->{extent}{minx} + $self->{parameters}{tilecol} * $width;
+        my $maxy = $projection->{extent}{maxy} - $self->{parameters}{tilerow} * $height;
+        my $maxx = $projection->{extent}{minx} + ($self->{parameters}{tilecol}+1) * $width;
+        my $miny = $projection->{extent}{maxy} - ($self->{parameters}{tilerow}+1) * $height;
 
-    if ($self->{parameters}{tilematrixset} eq 'EPSG:3857') {
-        $z = $self->{parameters}{tilematrix};
-        $y = $self->{parameters}{tilecol};
-        $x = 2**$z-$self->{parameters}{tilerow}-1;
-    }
-    
+        {
+            use bytes;
+            $file = "/vsistdout";
+            my $stdout = capture_stdout {
+                $ds->Translate($file, ['-of' => 'PNG', '-r' => 'bilinear' , 
+                                       '-outsize' , 256, 256, 
+                                       '-projwin', $minx, $maxy, $maxx, $miny]);
+            };
+        
+            return $self->{responder}->([ 200, 
+                                          [
+                                           'Content-Type'   => $set->{Format},
+                                           'Content-Length' => length($stdout)
+                                          ],
+                                          [$stdout],
+                                        ]);
+        }
 
-    my $file = "$set->{path}/$z/$y/$x.$set->{ext}";
-    $file = $self->{config}{blank} unless -r $file;
+    } else {
+
+        my $z = $self->{parameters}{tilematrix};
+        my $y = $self->{parameters}{tilecol};
+        my $x = 2**$z-$self->{parameters}{tilerow}-1;
+
+        $file = "$set->{path}/$z/$y/$x.$set->{ext}";
+        $file = $self->{config}{blank} unless -r $file;
+
+    }   
 
     open my $fh, "<:raw", $file or return $self->return_403;
 
@@ -308,6 +509,16 @@ sub GetTile {
                            $fh,
                          ]);
 }
+
+=pod
+
+=head3 TMS
+
+Sends TileMapService (layer not in the URL) or TileMap (layer in the
+URL but no zoom, row, col) document, or the requested tile based on
+layer, zoom, row, and column in the URL.
+
+=cut
 
 sub TMS {
     my ($self) = @_;
@@ -341,6 +552,31 @@ sub TMS {
                          ]);
 }
 
+sub tile_matrix_set {
+    my ($writer, $projection, $tile_matrix_set) = @_;
+    $writer->element('ows:Identifier' => $projection->{identifier});
+    $writer->element(BoundingBox => { crs =>  $projection->{crs} },
+                     [ ['ows:LowerCorner' => $projection->{extent}{minx}.' '.$projection->{extent}{miny} ],
+                       ['ows:UpperCorner' => $projection->{extent}{maxx}.' '.$projection->{extent}{maxy} ] ]);
+    $writer->element('ows:SupportedCRS' => { crs =>  $projection->{crs} });
+    my $extent_width = $projection->{extent}{maxx} - $projection->{extent}{minx};
+    for my $tile_matrix (@$tile_matrix_set) {
+        my $matrix_width = 2**$tile_matrix;
+        my $matrix_height = 2**$tile_matrix;
+        $writer->element(TileMatrix => 
+                         [ [ 'ows:Identifier' => $tile_matrix ],
+                           [ ScaleDenominator => 
+                             $extent_width / 
+                             ($matrix_width * $tile_width) / 
+                             $standard_pixel_size ],
+                           [ TopLeftCorner => $projection->{extent}{minx}.' '.$projection->{extent}{maxy} ],
+                           [ TileWidth => $tile_width ],
+                           [ TileHeight => $tile_height ],
+                           [ MatrixWidth => $matrix_width ],
+                           [ MatrixHeight => $matrix_height ] ]);
+    }
+}
+
 sub tilemaps {
     my ($self) = @_;
     my $writer = Geo::OGC::Service::XMLWriter::Caching->new();
@@ -371,8 +607,14 @@ sub tilemapresource {
     $writer->element(TileFormat => {width => 256, height => 256, 'mime-type' => $set->{Format}, extension => $ext });
     my @sets;
     my ($n, $m) = $set->{Resolutions} =~ /(\d+)\.\.(\d+)$/;
+    my $projection = $projections{$set->{SRS}};
+    my @resolutions;
+    my $extent_width = $projection->{extent}{maxx} - $projection->{extent}{minx};
+    for my $i (0..19) {
+        $resolutions[$i] = $extent_width/(2**$i * $tile_width);
+    }
     for my $i ($n..$m) {
-        push @sets, [TileSet => {href=>$i, order=>$i, 'units-per-pixel'=>$resolutions_3857[$i]}];
+        push @sets, [TileSet => {href=>$i, order=>$i, 'units-per-pixel'=>$resolutions[$i]}];
     }
     $writer->element(TileSets => {profile => "mercator"}, \@sets);
     $writer->close_element;
@@ -411,6 +653,12 @@ sub log {
 
 1;
 __END__
+
+=head1 LIMITATIONS
+
+Currently only EPSG 3067 (ETRS-TM35FIN) and 3857 (Google Mercator) are
+supported. To support other tile matrix sets add them to
+%Geo::OGC::Service::WMTS::projections.
 
 =head1 SEE ALSO
 
